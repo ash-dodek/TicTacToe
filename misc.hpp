55f4f40cad2ffd2cc4ce63f2b97e1364f33f39d0
pop builder <harshtrip2007@gmail.com>
2022-04-03 23:01:11 +0530

    fixing an issue

--- misc.hpp
+++ misc.hpp
@@ -3,28 +3,13 @@
 using namespace std;
 bool details() {
     clr();
-    cout<<"==================WELCOME TO THE CALCULATOR==================\n";
+    cout<<"==================WELCOME TO THE TICTACTOE GAME==================\n";
     cout<<"ENTER 1 =>  for 2 players\n";
     cout<<"ENTER 2 =>  for playing with robot\n";
     cout<<"ENTER 3 =>  for playing with robot(u can never win)\n";
-    cout<<"What do you want to play?";
-    // do{
-    //     cin>>ch;
-    //     if (ch==1) {
-    //         return true;
-    //     }
-    //     else if (ch==2) {
-    //         return true;
-    //     }
-    //     else if (ch==3) {
-    //         return true;
-    //     }
-    //     else{
-    //         cout<<"Hmmm....... seems like the number u entered is sussy, try again:";
-    //     }
-    // }while(ch<=0||ch>=4);
+    cout<<"Enter your choice: ";
 }
 
 void defCase() {
-    cout<<"This feature is being programmed, it will be released soon soon!\nMake sure to keep an eye on the repository";
+    cout<<"This feature is being programmed, it will be released soon soon!\nMake sure to keep an eye on the repository: https://github.com/Harsh69-bulider/TicTacToe.git\n";
 }
